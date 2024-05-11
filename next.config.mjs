dcc053b2004341c367425ac36eeb67fa446c9309
farrely-f <farrely.firenza@outlook.com>
2024-05-11 10:38:29 +0700

    feat: setup shadcn and prettier

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+  images: {
+    remotePatterns: [
+      {
+        hostname: "*",
+      },
+    ],
+  },
+};
 
 export default nextConfig;
